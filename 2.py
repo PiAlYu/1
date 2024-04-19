4430cbee136f2d2192e7c8ca2e3ccbf3996d3fb6
PiAlYu <157111194+PiAlYu@users.noreply.github.com>
2024-04-19 12:48:58 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,30 @@
+from math import ceil
+
+fin = open('27B_15342.txt')
+a = fin.readlines()
+fin.close()
+a = [list(map(int, i.split())) for i in a]
+n, k = a.pop(0)
+m = float('inf')
+for i in range(n):
+    a[i][1] = ceil(a[i][1] / 11)
+b = [0] * k
+for i in range(n):
+    b[a[i][0]] = a[i][1]
+baza, km, pv = b[0], 0, 0
+for i in range(len(b)):
+    r = abs(i - km)
+    pv += min(k - r, r) * b[i]
+ps = [b[0]]
+for i in range(1, len(b)):
+    ps.append(ps[-1] + b[i])
+for i in range(1, len(b)):
+    if i <= k // 2:
+        minus = ps[i - 1 + k // 2] - ps[i - 1]
+        plus = ps[-1] - minus
+    else:
+        plus = ps[i - 1] - ps[i - 1 - k // 2]
+        minus = ps[-1] - plus
+    pv = pv - minus + plus
+    m = min(m, pv)
+print(m)
